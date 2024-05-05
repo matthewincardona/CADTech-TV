c4aed42a81faa5dc85647ce97df325454439128f
matthewincardona <doublethink.inc@gmail.com>
2024-05-05 11:54:19 -0400

    Add schedule page

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import csv
+
+schedule_data = []
+
+with open('schedule/Monday.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+
+    for row in reader:
+        schedule_data.append(row)
+    
+    print(schedule_data)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def hello_world(schedule=schedule_data):
+    return render_template('index.html', schedule=schedule)
